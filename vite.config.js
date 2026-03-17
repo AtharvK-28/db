e058542190b05980de40d5c490e28a9db10802d0
Atharv <atharvk280206@gmail.com>
2026-03-17 11:32:07 +0530

    Final Changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This allows Vite to accept connections from your specific Ngrok URL
+    allowedHosts: [
+      'overwidely-pseudoprofessorial-blondell.ngrok-free.dev'
+    ],
+    // Optional hackathon tip: 'host: true' makes sure Vite listens on all network interfaces
+    host: true 
+  }
 })
